8db139b1b3f01770e509f5ec94d5d8107a731745
Prashanth <imprash4u@gmail.com>
2021-09-08 13:43:19 +0530

    Added: Insert Element after FirstNode

--- src/main/java/com/bridgelabz/linkedliist/LinkedList.java
+++ src/main/java/com/bridgelabz/linkedliist/LinkedList.java
@@ -4,7 +4,7 @@ public class LinkedList {
     Node head;
     Node tail;
 
-    public void push(int data) {
+    public Node push(int data) {
         Node newNode = new Node(data);
         if (head == null) {
             head = newNode;
@@ -13,7 +13,7 @@ public class LinkedList {
             Node temp = head;
             this.head = newNode;
             newNode.next = temp;
-        }
+        }return newNode;
     }
 
     public void print() {
@@ -40,6 +40,11 @@ public class LinkedList {
             this.tail.next = newNode;
             tail = newNode;
         }
+    }
+    public void insertInBetween(Node previousNode ,Node newNode){
+        Node tempNode = previousNode.next;
+        previousNode.next = newNode;
+        newNode.next = tempNode;
     }
 }
 
