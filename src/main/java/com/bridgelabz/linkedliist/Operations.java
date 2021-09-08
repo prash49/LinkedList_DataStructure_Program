abaeabe3002a3d77f65a410e96046ecdd174eab0
Prashanth <imprash4u@gmail.com>
2021-09-08 13:18:36 +0530

    Added: Inserting Element/data at End

--- src/main/java/com/bridgelabz/linkedliist/Operations.java
+++ src/main/java/com/bridgelabz/linkedliist/Operations.java
@@ -2,13 +2,21 @@ package com.bridgelabz.linkedliist;
 
 
 public class Operations {
-    public static void addDataAtStart(){
-        LinkedList linkedList= new LinkedList();
+    public static void addDataAtStart() {
+        LinkedList linkedList = new LinkedList();
         linkedList.push(70);
         linkedList.push(30);
         linkedList.push(56);
         linkedList.print();
 
 
+    }
+
+    public static void addDataAtEnd() {
+        LinkedList linkedList = new LinkedList();
+        linkedList.append(56);
+        linkedList.append(30);
+        linkedList.append(70);
+        linkedList.print();
     }
 }
