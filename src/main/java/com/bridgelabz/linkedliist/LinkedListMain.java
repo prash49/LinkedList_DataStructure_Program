afb90248aa42f6ea12bfd639a7fc1f846180af70
Prashanth <imprash4u@gmail.com>
2021-09-08 12:10:33 +0530

    Added: Created a LinkedList with data

--- src/main/java/com/bridgelabz/linkedliist/LinkedListMain.java
+++ src/main/java/com/bridgelabz/linkedliist/LinkedListMain.java
@@ -2,12 +2,22 @@ package com.bridgelabz.linkedliist;
 
 /**
  * Hello world!
- *
  */
-public class LinkedListMain
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Welcome to linkedList DataStructure Program" );
+public class LinkedListMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to linkedList DataStructure Program");
+        Node firstNode = new Node(56);
+        Node secondNode = new Node(30);
+        Node thirdNode = new Node(70);
+        Node head = firstNode;
+        firstNode.next = secondNode;
+        secondNode.next = thirdNode;
+        Node tail = thirdNode;
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
     }
 }
